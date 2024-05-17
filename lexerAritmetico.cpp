4d88ca31550cb4aa50d0f4e7f33349b0b85802a0
BrunoGaxiola <A01253874tec.mx>
2024-05-16 17:48:54 -0700

    add: código con lo que falta.

--- lexerAritmetico.cpp
+++ lexerAritmetico.cpp
@@ -4,96 +4,127 @@
 #include <vector>
 #include <regex>
 
-using namespace std;
-
+// Enumeración que representan los tipos de Tokens.
 enum TokenType {
-    INTEGER,
-    FLOAT,
-    OPERATOR,
-    IDENTIFIER,
-    SPECIAL_SYMBOL,
-    COMMENT,
-    UNKNOWN
+    Entero,
+    Flotante,
+    Asignacion,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Potencia,
+    Variable,
+    ParentesisIzquierdo,
+    ParentesisDerecho,
+    Comentario
 };
 
+// Estructura del Token.
 struct Token {
-    string value;
-    TokenType type;
+    std::string valor;
+    TokenType tipo;
 };
 
-vector<Token> lexerAritmetico(string archivo) {
-    ifstream file(archivo);
-    string line;
-    vector<Token> tokens;
+std::vector<Token> lexer(const std::string& linea) {
+    std::vector<Token> tokens;
+    std::stringstream ss(linea);
+    std::string palabra;
+    std::regex entero_regex("^[0-9]+$");
+    std::regex flotante_regex("^[0-9]*\\.[0-9]+([eE][-+]?[0-9]+)?$");
+    std::regex variable_regex("^[a-zA-Z_][a-zA-Z0-9_]*$");
 
-    regex integer_regex("^-?[0-9]+$");
-    regex float_regex("^-?[0-9]*\\.[0-9]+([eE][-+]?[0-9]+)?$");
-    regex operator_regex("^[=+\\-*/^]$");
-    regex identifier_regex("^[a-zA-Z_][a-zA-Z0-9_]*$");
-    regex special_symbol_regex("^[()]$");
-    regex comment_regex("^//.*$");
-
-    while (getline(file, line)) {
-        istringstream iss(line);
-        string word;
-        bool inComment = false;
-        while (getline(iss, word, ' ')) {
-            if (!inComment && word.find("//") == 0) {
-                inComment = true;
-                continue;
-            }
-            if (inComment) {
-                if (word.find("\n")!= string::npos) {
-                    inComment = false;
-                }
-                continue;
-            }
-            for (char& c : word) {
-                string str(1, c);
-                Token token;
-                token.value = str;
-                if (regex_match(str, integer_regex)) {
-                    token.type = INTEGER;
-                } else if (regex_match(str, float_regex)) {
-                    token.type = FLOAT;
-                } else if (regex_match(str, operator_regex)) {
-                    token.type = OPERATOR;
-                } else if (regex_match(str, identifier_regex)) {
-                    token.type = IDENTIFIER;
-                } else if (regex_match(str, special_symbol_regex)) {
-                    token.type = SPECIAL_SYMBOL;
-                } else {
-                    token.type = UNKNOWN;
-                }
-                tokens.push_back(token);
-            }
+    while (ss >> palabra) {
+        if (regex_match(palabra, entero_regex)) {
+            tokens.push_back({palabra, Entero});
+        } else if (regex_match(palabra, flotante_regex)) {
+            tokens.push_back({palabra, Flotante});
+        } else if (palabra == "=") {
+            tokens.push_back({palabra, Asignacion});
+        } else if (palabra == "+") {
+            tokens.push_back({palabra, Suma});
+        } else if (palabra == "-") {
+            tokens.push_back({palabra, Resta});
+        } else if (palabra == "*") {
+            tokens.push_back({palabra, Multiplicacion});
+        } else if (palabra == "/") {
+            tokens.push_back({palabra, Division});
+        } else if (palabra == "^") {
+            tokens.push_back({palabra, Potencia});
+        } else if (palabra == "(") {
+            tokens.push_back({palabra, ParentesisIzquierdo});
+        } else if (palabra == ")") {
+            tokens.push_back({palabra, ParentesisDerecho});
+        } else if (palabra.substr(0, 2) == "//") {
+            std::string comentario = linea.substr(linea.find("//"));
+            tokens.push_back({comentario, Comentario});
+            break;  // Recorre todo lo que queda de la línea para conseguir todo el comentario.
+        } else if (regex_match(palabra, variable_regex)) {
+            tokens.push_back({palabra, Variable});
         }
     }
-
     return tokens;
 }
 
-int main() {
-    string archivo = "expresion.txt";
-    cout << "Leyendo el archivo: " << archivo << endl;
-    vector<Token> tokens = lexerAritmetico(archivo);
-
-    cout << "Tokens encontrados: " << tokens.size() << endl;
-
-    for (Token token : tokens) {
-        cout << "Token: " << token.value << ", Tipo: ";
-        switch (token.type) {
-            case INTEGER: cout << "Entero"; break;
-            case FLOAT: cout << "Real"; break;
-            case OPERATOR: cout << "Operador"; break;
-            case IDENTIFIER: cout << "Variable"; break;
-            case SPECIAL_SYMBOL: cout << "Símbolo especial"; break;
-            case COMMENT: cout << "Comentario"; break;
-            case UNKNOWN: cout << "Desconocido"; break;
-        }
-        cout << endl;
+// Función principal.
+void lexerAritmetico(std::string archivo) {
+    std::ifstream file(archivo);
+    if (!file.is_open()) {
+        std::cerr << "No se pudo abrir el archivo: " << archivo << std::endl;
+        return;
     }
 
+    std::string linea;
+    while (getline(file, linea)) {
+        std::vector<Token> tokens = lexer(linea);
+        for (const Token& token : tokens) {
+            switch (token.tipo) {
+                case Entero:
+                    std::cout << token.valor << "  Entero" << std::endl;
+                    break;
+                case Flotante:
+                    std::cout << token.valor << "  Flotante" << std::endl;
+                    break;
+                case Asignacion:
+                    std::cout << token.valor << "  Asignacion" << std::endl;
+                    break;
+                case Suma:
+                    std::cout << token.valor << "  Suma" << std::endl;
+                    break;
+                case Resta:
+                    std::cout << token.valor << "  Resta" << std::endl;
+                    break;
+                case Multiplicacion:
+                    std::cout << token.valor << "  Multiplicacion" << std::endl;
+                    break;
+                case Division:
+                    std::cout << token.valor << "  Division" << std::endl;
+                    break;
+                case Potencia:
+                    std::cout << token.valor << "  Potencia" << std::endl;
+                    break;
+                case Variable:
+                    std::cout << token.valor << "  Variable" << std::endl;
+                    break;
+                case ParentesisIzquierdo:
+                    std::cout << token.valor << "  Parentesis Izquierdo" << std::endl;
+                    break;
+                case ParentesisDerecho:
+                    std::cout << token.valor << "  Parentesis Derecho" << std::endl;
+                    break;
+                case Comentario:
+                    std::cout << token.valor << "  Comentario" << std::endl;
+                    break;
+            }
+        }
+    }
+
+    file.close();
+}
+
+int main() {
+    std::string archivo = "expresiones.txt";
+    lexerAritmetico(archivo);
     return 0;
 }
 
